482014def8fd1d7e257c0c005ac7ed40d090e466
Misha Goncharov <forsomeletters@gmail.com>
2021-09-23 02:13:22 +0300

    Add comments to the Webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path')
 
 module.exports = {
+    // Consider index.ts as an entry point
     entry: './src/index.ts',
+
+    // Specify the rules for handling different file types
     module: {
         rules: [
             {
+                // Find all .tsx files except those in /node_modules/ folder. Process them using ts-loader
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
             }
         ]
     },
+
     resolve: {
+        // If multiple files share the same name but have different extensions, webpack will resolve the one with the extension listed first in the array and skip the rest: https://webpack.js.org/configuration/resolve/#resolveextensions. This list will enable us to leave off the extension when importing
         extensions: ['.ts', '.tsx', '.js']
     },
+
+    // Save the results in index.js file located in /dist
     output: {
         filename: 'index.js',
         path: path.resolve('dist')
