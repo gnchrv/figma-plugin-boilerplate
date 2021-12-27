09f313ab63a9f1d97f5c8f592645256943100489
Misha Goncharov <forsomeletters@gmail.com>
2021-12-27 18:55:17 +0300

    Add a close command, so the template plugin would close automatically after execution

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-if (figma.command == 'first-command')
+if (figma.command == 'first-command') {
     console.log('The first command was executed')
+    figma.closePlugin()
+}
 
-if (figma.command == 'second-command')
+if (figma.command == 'second-command') {
     console.log('The second command was executed')
+    figma.closePlugin()
+}
 
